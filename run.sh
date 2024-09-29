4311b52fc6310276a3426fe03ad021b534b60749
tp <todd@killercloud.com.au>
2024-09-29 17:08:37 +1000

    ddcdd

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export RESOURCE_GROUP="kc-aci-rig"
 export LOCATION="australiaeast"
 export ENVIRONMENT="webdev-appenv"
 export GITHUB_USERNAME="tp12121212"
-export ACR_NAME="kcacrnew"$GITHUB_USERNAME
+export ACR_NAME="kcacr"$GITHUB_USERNAME
 
 
 az containerapp up --name $APP_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --environment $ENVIRONMENT --context-path ./src --repo php-apache-docker
