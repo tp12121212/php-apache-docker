219ea979a75a3b956ac00409b0f1abafbdb5a2b4
tp <todd@killercloud.com.au>
2024-09-29 14:59:30 +1000

    dddd

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ export RESOURCE_GROUP="kc-aci-rig"
 export LOCATION="australiaeast"
 export ENVIRONMENT="webdev-appenv"
 export GITHUB_USERNAME="tp12121212"
-export ACR_NAME="kcacr"$GITHUB_USERNAME
+export ACR_NAME="kcacrnew"$GITHUB_USERNAME
